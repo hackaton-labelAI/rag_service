727e121767be0dd8e3efdd0e373282d23fe9e2a2
KilArtur <kil_artur@inbox.ru>
2024-10-12 00:52:50 +0300

    update

--- services/answer_service.py
+++ services/answer_service.py
@@ -1,7 +1,10 @@
+import asyncio
+import json
 from typing import List
 
 from endpoints.models.chat_context import ChatContext
 from services.find_service import ranking, find_chunks
+from services.gpt_service import fetch_completion
 
 
 def answer_in_chat(chat_context: ChatContext):
@@ -15,6 +18,31 @@ def answer_in_chat(chat_context: ChatContext):
     pass
 
 
-def resolve_issue(question: str)-> List[str]:
-    pass
+async def resolve_issue(question: str)-> List[str]:
+    arr_questions = []
 
+    prompt = """
+      Необходимо придумать 4 других интерпритации вопроса и записать их в массив
+      Вопрос:
+      %s
+
+      В ответе должен быть массив из 5 вопросов: изначальный вопрос и других 4 перефразированных вопросов:
+      json
+      {
+        'arr': <пересчитаное количество>
+      }
+      """
+    res = await fetch_completion(prompt % (str(question)))
+
+    try:
+        json_data = json.loads(res['full_text'])
+        # print(json_data)
+    except json.JSONDecodeError as e:
+        print(f"Ошибка при преобразовании в JSON: {e}")
+        json_data = None
+
+    # print(json(res['full_text']))
+    return json_data['arr']
+# result = asyncio.run(resolve_issue('Есть ли у меня привилегии если я работаю в РЖД?'))
+
+# print(result)
---
+++ services/gpt_service.py
@@ -0,0 +1,33 @@
+import json
+import openai
+from openai import AsyncOpenAI
+
+TOKEN = '1000097868.110610.cfUF9zxN1MD7Ot8IHmr48LLXdtDv9Do9T9RZkfSL'
+openai = AsyncOpenAI(
+    api_key=TOKEN,
+    base_url="https://caila.io/api/adapters/openai"
+)
+
+async def fetch_completion(prompt: str):
+    try:
+        res = await openai.chat.completions.create(
+            messages=[{"role": "user", "content": prompt}],
+            model="just-ai/openai-proxy/gpt-4o",
+            temperature=0,
+            response_format={"type": "json_object"},
+            stream=False
+        )
+
+        response_json = res
+        input_tokens = int(res.usage.prompt_tokens)
+        output_tokens = int(res.usage.completion_tokens)
+        content = res.choices[0].message.content
+
+        return {
+            "response_json": response_json,
+            "input_tokens": input_tokens,
+            "output_tokens": output_tokens,
+            "full_text": content
+        }
+    except Exception as e:
+      print(f"error {e}")
