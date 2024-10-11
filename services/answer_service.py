560f98979b16cdb31b68ed8387de74953b8d3536
KilArtur <kil_artur@inbox.ru>
2024-10-12 01:04:30 +0300

    update

--- services/answer_service.py
+++ services/answer_service.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
 from typing import List
-
+from prompts import prompt_multiplication
 from endpoints.models.chat_context import ChatContext
 from services.find_service import ranking, find_chunks
 from services.gpt_service import fetch_completion
@@ -21,18 +21,7 @@ def answer_in_chat(chat_context: ChatContext):
 async def resolve_issue(question: str)-> List[str]:
     arr_questions = []
 
-    prompt = """
-      Необходимо придумать 4 других интерпритации вопроса и записать их в массив
-      Вопрос:
-      %s
-
-      В ответе должен быть массив из 5 вопросов: изначальный вопрос и других 4 перефразированных вопросов:
-      json
-      {
-        'arr': <пересчитаное количество>
-      }
-      """
-    res = await fetch_completion(prompt % (str(question)))
+    res = await fetch_completion(prompt_multiplication % (str(question)))
 
     try:
         json_data = json.loads(res['full_text'])
@@ -43,6 +32,6 @@ async def resolve_issue(question: str)-> List[str]:
 
     # print(json(res['full_text']))
     return json_data['arr']
-# result = asyncio.run(resolve_issue('Есть ли у меня привилегии если я работаю в РЖД?'))
+result = asyncio.run(resolve_issue('Есть ли у меня привилегии если я работаю в РЖД?'))
 
-# print(result)
+print(result)
