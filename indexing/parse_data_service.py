8274ea0db51c0735f24f4f85afd72705bfac31ae
KilArtur <kil_artur@inbox.ru>
2024-10-12 21:35:11 +0300

    update

--- indexing/parse_data_service.py
+++ indexing/parse_data_service.py
@@ -1,33 +1,35 @@
 import re
 from typing import List
-
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from pydantic.dataclasses import dataclass
-import tiktoken
+import json
+from transformers import AutoTokenizer
 
 
 @dataclass
-class ChunkData:
-    char_position_start: int
-    char_position_end: int
-    chunk_text: str
-    chapter: str
+class dr:
+    version: int
+    label: str
+    text: str
 
+@dataclass
+class ChunkData:
+    chunk_text: str
+    data: List[dr]
 
 @dataclass
 class ReturnFormat:
     chunk_text: List[ChunkData]
     full_chapter_text: str
 
-
-tokenizer = tiktoken.get_encoding("cl100k_base")
+# Загружаем токенизатор BERT
+bert_tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
 def token_length_function(text):
     """Функция для подсчета количества токенов в тексте."""
-    tokens = tokenizer.encode(text)
+    tokens = bert_tokenizer.encode(text, truncation=False)
     return len(tokens)
-
 
 def parsing_data_from_pdf(file_name: str, token_chunk: int = 250, overlay: int = 50) -> List[ReturnFormat]:
     file_path = f'../data/{file_name}'
@@ -35,52 +37,57 @@ def parsing_data_from_pdf(file_name: str, token_chunk: int = 250, overlay: int =
     loader = PyPDFLoader(file_path)
     file = loader.load()
     content = ''.join(file[i].page_content for i in range(len(file)))
-    return parsing_data_from_text(content, token_chunk, overlay)
-
-
-
-def parsing_data_from_text(content: str, token_chunk: int = 250, overlay: int = 50) -> List[ReturnFormat]:
-    pattern = r'(Раздел \d+)(.*?)(?=Раздел \d+|$)'
+    pattern = r'(Раздел \d+\.\s[^\n]+)(.*?)(?=Раздел \d+\.|$)'
     sections = re.findall(pattern, content, re.DOTALL)
-
     texts = [{'label': section[0], 'text': section[1].strip()} for section in sections]
 
-    chunk_data_list = []
+    result = []
+    for text in texts:
+        parsed_data = parsing_data_from_text([text], token_chunk, overlay)
+        result.extend(parsed_data)
+    return result
 
+def parsing_data_from_web(token_chunk: int = 450, overlay: int = 100) -> List[ReturnFormat]:
+    loader = WebBaseLoader(['https://company.rzd.ru/ru/9353/page/105104?id=1604#navPart_7274'])
+    file = loader.load()
+    content = ''.join(file[i].page_content for i in range(len(file)))
+    pattern = r'(Раздел \d+\.\s[^\n]+)(.*?)(?=Раздел \d+\.|$)'
+    sections = re.findall(pattern, content, re.DOTALL)
+
+    texts = [{'label': section[0].strip(), 'text': section[1].strip()} for section in sections if section[1].strip()]
+
+    result = []
+    for text in texts:
+        parsed_data = parsing_data_from_text([text], token_chunk, overlay)
+        result.extend(parsed_data)
+
+    return result
+
+def parsing_data_from_text(content: List[dict], token_chunk: int = 450, overlay: int = 100) -> List[ReturnFormat]:
     all_data = []
 
-    for text in texts:
+    for text in content:
+        chunk_data_list = []
+
+        # Создание объекта RecursiveCharacterTextSplitter
         text_splitter = RecursiveCharacterTextSplitter(
-            separators=["\n\n", "\n", " ", ".", ",", ";"],
             chunk_size=token_chunk,
             chunk_overlap=overlay,
-            length_function=token_length_function,
-            is_separator_regex=False,
-            add_start_index=True
+            length_function=token_length_function
         )
-        text_chunks = text_splitter.split_text(text['text'])
 
-        current_position = 0
+        # Разделение текста на чанки
+        chunks = text_splitter.split_text(text['text'])
+        for chunk in chunks:
+            chunk = chunk.strip()  # Удаляем лишние пробелы
 
-        for chunk in text_chunks:
-            # Получаем начало и конец чанка в оригинальном тексте
-            start_char_idx = content.find(chunk, current_position)
-            end_char_idx = start_char_idx + len(chunk)
-
-            # Если не нашли кусок, продолжаем
-            if start_char_idx == -1:
-                print(f"Chunk not found in content: {chunk}")
-                continue
-
-            chunk_data = ChunkData(
-                char_position_start=start_char_idx,
-                char_position_end=end_char_idx,
-                chunk_text=chunk,
-                chapter=text['label']
-            )
-            chunk_data_list.append(chunk_data)
-
-            current_position = end_char_idx
+            # Добавляем чанк, только если он не пустой и имеет осмысленное содержание
+            if len(chunk) > 10 and not chunk.startswith("Раздел"):  # Избегаем заголовков и слишком коротких чанков
+                chunk_data = ChunkData(
+                    chunk_text=chunk,
+                    data=[dr(version=1, label=text['label'], text=chunk)]
+                )
+                chunk_data_list.append(chunk_data)
 
         all_data.append(ReturnFormat(
             chunk_text=chunk_data_list,
@@ -89,13 +96,30 @@ def parsing_data_from_text(content: str, token_chunk: int = 250, overlay: int = 
 
     return all_data
 
-#  APPROVED
-if __name__ == "__main__":
-    result = parsing_data_from_pdf('dd.pdf')
-    for i, return_format in enumerate(result):
-        print(f"Total chunks in section {i}: {len(return_format.chunk_text)}")
-        for chunk in return_format.chunk_text:
-            print(f"Chunk Text: {chunk.chunk_text}, Start: {chunk.char_position_start}, End: {chunk.char_position_end}, Chapter: {chunk.chapter}")
-            print(f"Full Chapter Text: {return_format.full_chapter_text}")
+def format_result_to_json(result: List[ReturnFormat]) -> List[dict[str, any]]:
+    formatted_result = {}
 
-# print(f"all_data must be 12, but real: {len(result)}")
+    for return_format in result:
+        for chunk in return_format.chunk_text:
+            original_uuid = chunk.data[0].label
+
+            if original_uuid not in formatted_result:
+                formatted_result[original_uuid] = {
+                    "doc_id": "1",  # фиксируем значение
+                    "content": return_format.full_chapter_text,
+                    "chunks": [],
+                }
+
+            formatted_result[original_uuid]["chunks"].append({
+                "chunk_id": f"doc_1_chunk_{len(formatted_result[original_uuid]['chunks'])}",
+                "original_index": len(formatted_result[original_uuid]["chunks"]),
+                "content": chunk.chunk_text,
+                "version": "1"
+            })
+
+    return list(formatted_result.values())
+
+if __name__ == "__main__":
+    result = parsing_data_from_web()
+    formatted_json = format_result_to_json(result)
+    print(json.dumps(formatted_json, indent=2, ensure_ascii=False))
